dccf586afa2bb2172cc541852b12a34508469b18
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-08-18 23:21:49 +0500

    Deploy React app to github

---
+++ build/precache-manifest.db8fc1ec47caf272b67a37ee4bec80e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d10a5dc48abc50213f0e11e5cb9840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab13da77be872f8bf653",
+    "url": "/static/css/main.f2ce5034.chunk.css"
+  },
+  {
+    "revision": "2b4c1199f1e002dcdb8b",
+    "url": "/static/js/2.e1eeb85d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1eeb85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab13da77be872f8bf653",
+    "url": "/static/js/main.a422130c.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
